0a60860c95868f7400c92ea3857585c85fa333d5
amama19862024-ui <amama19862024@gmail.com>
2026-06-02 19:59:39 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,22 +1,17 @@
-// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
-// あなたのFirebase設定
 const firebaseConfig = {
   apiKey: "AIzaSyA3CIViJgCoyP-WCs_YFAFchlLWKxoFM-Y",
   authDomain: "progresso-ozasa.firebaseapp.com",
   projectId: "progresso-ozasa",
   storageBucket: "progresso-ozasa.firebasestorage.app",
   messagingSenderId: "924703297034",
-  appId: "1:924703297034:web:8cebcf3c37a71711e6d9f3",
-  measurementId: "G-R4F5Q86WS3"
+  appId: "1:924703297034:web:8cebcf3c37a71711e6d9f3"
 };
 
-// Firebase初期化
 const app = initializeApp(firebaseConfig);
 
-// Firestore
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
