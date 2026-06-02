3489a357c7ef41e3c3278ade3e712c826a77681b
amama19862024-ui <amama19862024@gmail.com>
2026-06-02 17:24:50 +0900

    Firestore連携

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
 import { db } from "./firebase.js";
 
-console.log("プログレッソ小笹アプリ起動");
-console.log("Firestore接続OK", db);
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+async function loadAnnouncements() {
+
+  const container =
+    document.getElementById("announcements");
+
+  try {
+
+    const snapshot =
+      await getDocs(collection(db, "announcements"));
+
+    let html = "";
+
+    snapshot.forEach((doc) => {
+
+      const data = doc.data();
+
+      html += `
+        <div>
+          <h3>${data.title}</h3>
+          <p>${data.content}</p>
+          <hr>
+        </div>
+      `;
+    });
+
+    container.innerHTML = html;
+
+  } catch (error) {
+
+    console.error(error);
+
+    container.innerHTML =
+      "お知らせの取得に失敗しました";
+  }
+}
+
+loadAnnouncements();
